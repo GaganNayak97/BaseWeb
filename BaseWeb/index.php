b3404b8219106b20151328d040a58064b3661101
Eliost Team <gagannayak396@gmail.com>
2026-01-22 23:01:54 +0530

    Initial setup project

---
+++ BaseWeb/index.php
@@ -0,0 +1,5 @@
+<?php
+define("BASE_PATH", __DIR__);
+define("ASSETS", "/assets");
+
+include BASE_PATH . "/BaseWeb/web/Xor/vr-01/Theme_Basic/User/index.php";
---
+++ BaseWeb/web/Xor/vr-01/Theme_Basic/User/index.php
@@ -0,0 +1,21 @@
+<html>
+    <head>
+		<title>Xor</title>
+		
+		<!-- Chrome, Firefox OS and Opera --> 	
+		<meta name="theme-color" content="#ffffff">
+		<!-- Windows Phone -->
+		<meta name="msapplication-navbutton-color" content="#ffffff">
+		<!-- iOS Safari -->
+		<meta name="apple-mobile-web-app-status-bar-style" content="#ffffff">
+		<meta name="copyright" content="Copyright 2020-2025. E-Commerce. All rights reserved.">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">	
+		<link rel="icon" type="image/png" href="">
+		<link rel="icon" href="" sizes="32x32">	
+		<link rel="stylesheet" href="/GitHub/XorAi/Xor/css&amp;v=33">
+		<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>	
+	</head>
+    <body>
+
+    </body>
+</html>
