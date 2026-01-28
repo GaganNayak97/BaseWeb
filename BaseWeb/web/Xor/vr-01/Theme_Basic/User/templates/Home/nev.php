6ac2a9df19c70563771cfc02b5569a84f934db9d
Eliost Team <gagannayak396@gmail.com>
2026-01-28 17:07:39 +0530

    Update content layout and templates include
    Fixed include paths and organized user templates structure.

---
+++ BaseWeb/web/Xor/vr-01/Theme_Basic/User/templates/Home/nev.php
@@ -0,0 +1,26 @@
+ <nev>
+        <d class="nev">
+          <cnvo uti="nev">
+            <d>
+              <hd>
+                <t>
+                  <name>XOR.ai</name>
+                  <vr>0.1</vr>
+                </t>
+              </hd>
+              <bd></bd>
+              <ft>
+                <btns>
+                  <btn>
+                    <i><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="none" d="M0 0h24v24H0V0z"/><path d="M18 13h-5v5c0 .55-.45 1-1 1s-1-.45-1-1v-5H6c-.55 0-1-.45-1-1s.45-1 1-1h5V6c0-.55.45-1 1-1s1 .45 1 1v5h5c.55 0 1 .45 1 1s-.45 1-1 1z"/></svg></i>
+                    <t>Share</t>
+                  </btn>
+                  <op>
+                    <i><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="none" d="M0 0h24v24H0V0z"/><path d="M18 13h-5v5c0 .55-.45 1-1 1s-1-.45-1-1v-5H6c-.55 0-1-.45-1-1s.45-1 1-1h5V6c0-.55.45-1 1-1s1 .45 1 1v5h5c.55 0 1 .45 1 1s-.45 1-1 1z"/></svg></i>
+                  </op>
+                </btns>
+              </ft>
+            </d>
+          </cnvo>
+        </d>
+      </nev>
